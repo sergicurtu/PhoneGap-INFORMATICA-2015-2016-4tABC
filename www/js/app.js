1702614216be2a294d6ea806a9cf345316132d68
sergicurtu <sergi.curtu@numancia.cat>
2015-12-12 19:15:55 +0100

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,23 @@
 window.addEventListener('load', function() {
     
+    // Initialize collapse button
+      $(".button-collapse").sideNav();
+      
+      
+      $("#nav_lateral").click(function(){
+        
+            $('.button-collapse').sideNav('show');
+      
+      });
+      
+      $(".dia_menu_clicat").click(function(){
+        
+            var dia_clicat = $(this).attr('dia');
+            alert("Clicat el " + dia_clicat);
+      
+      });
+    
+    
      $JSView.declareMenu({
         
         viewMenuA: {
