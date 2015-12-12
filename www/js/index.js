41902f6bb7126a1755210d39f7e8c39cea9c7a53
sergicurtu <sergi.curtu@numancia.cat>
2015-12-12 19:57:23 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,12 +3,13 @@ $(document).on('deviceready', function() {
     // Initialize collapse button
       $(".button-collapse").sideNav();
       
-      
+      /* 
       $("#nav_lateral").click(function(){
         
             $(".button-collapse").sideNav("show");
       
       });
+      */
       
       $(".dia_menu_clicat").click(function(){
         
@@ -16,6 +17,12 @@ $(document).on('deviceready', function() {
             alert("Clicat el " + dia_clicat);
       
       });
+
+      $(".Diumenge").click(function(){
+        
+            var dia_clicat = $(this).attr("dia");
+            alert("Clicat el " + dia_clicat);
       
+      });      
       
 });
