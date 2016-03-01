1dcfb89d617735d4ba0dc8a37c2598402f68007e
sergicurtu <sergi.curtu@numancia.cat>
2016-03-01 21:24:31 +0100

    afegits mapes ( finestra emergent )

--- www/js/app.js
+++ www/js/app.js
@@ -73,6 +73,36 @@ window.addEventListener('load', function() {
             url: '/modal_info_aeroport',
             template: 'views/modal_info_aeroport.html',
             controller: 'modal_info_aeroport'
+        },
+        modal_info_coliseo: {
+            url: '/modal_info_coliseo',
+            template: 'views/modal_info_coliseo.html',
+            controller: 'modal_info_coliseo'
+        },
+        modal_info_narvona: {
+            url: '/modal_info_narvona',
+            template: 'views/modal_info_narvona.html',
+            controller: 'modal_info_narvona'
+        },
+        modal_info_sanpietro: {
+            url: '/modal_info_sanpietro',
+            template: 'views/modal_info_sanpietro.html',
+            controller: 'modal_info_sanpietro'
+        },
+        mapa_roma: {
+            url: '/mapa_roma',
+            template: 'views/mapa_roma.html',
+            controller: 'mapa_roma'
+        },
+        mapa_florencia: {
+            url: '/mapa_florencia',
+            template: 'views/mapa_florencia.html',
+            controller: 'mapa_florencia'
+        },
+        mapa_venecia: {
+            url: '/mapa_venecia',
+            template: 'views/mapa_venecia.html',
+            controller: 'mapa_venecia'
         }
     });
      
